85ae4ac505e0dcf90cab7c997738067cbbb15ab5
MOHAMMED IT <101934644+Barzaaniy@users.noreply.github.com>
2022-03-20 21:01:13 +0300

    yakam fileman dana
    takw fer bbin yakam fileman dana

---
+++ simple calculator for cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a , b ;
+    cout<<"zhmarae yakam daxl bka:";
+    cin>>a;
+    cout<<"zhmarae dwam daxl bka:";
+    cin>>b;
+    cout<<a+b;
+    return 0;
+}
